b199022d406f8b98a0b42662a7585b50b8bd4fe0
vagner ventura <vagnerventura@hotmail.com>
2025-06-28 09:29:46 -0300

    fix: correção bug CORS

--- src/main/java/com/LaMusic/entity/Product.java
+++ src/main/java/com/LaMusic/entity/Product.java
@@ -1,16 +1,30 @@
 package com.LaMusic.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.UUID;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "products")
@@ -28,6 +42,7 @@ public class Product {
 
     private String name;
     private String slug;
+    
     private String description;
 
     @Column(name = "short_description")
